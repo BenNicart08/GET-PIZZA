a1e3c8474511fc649394c5e69e3002a549b8afdd
Daniel-1125 <javierdaniel1125@gmail.com>
2024-08-19 15:42:53 +0800

    about mobile done

---
+++ dist/js/slider.js
@@ -0,0 +1,12 @@
+var slider = tns({
+  container: ".feedback__slider",
+  items: 1,
+  speed: 300,
+  gutter: 100,
+  slideBy: "page",
+  autoplay: true,
+  nav: "false",
+    controls: false,
+    autoplayButtonOutput: false,
+  
+});
